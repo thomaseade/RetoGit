173133670c4759011fb2740d9536169ccbb049c9
thomaseade <eadethomas95@gmail.com>
2023-03-14 16:46:24 +0100

    reto2

---
+++ index.js
@@ -0,0 +1,25 @@
+function suma(op1, op2) {
+    return op1 + op2;
+  }
+  
+  function resta(op1, op2) {
+    return op1 - op2;
+  }
+  
+  function producto(op1, op2) {
+    return op1 * op2;
+  }
+  
+  function division(op1, op2) {
+    return op1 / op2;
+  }
+
+  function cuadrado(op1) {
+    return op1 * op1;
+  }
+  
+  
+
+
+
+module.exports = { suma, resta, producto, division, cuadrado };
---
+++ run.js
@@ -0,0 +1,7 @@
+let myLib = require("./index");
+
+console.log(myLib.suma(7, 1));
+console.log(myLib.resta(3, 1));
+console.log(myLib.producto(2, 3));
+console.log(myLib.division(8, 2));
+console.log(myLib.cuadrado(4));
