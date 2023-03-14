64d6938ca8231e6fa03ef7eecbf2c2b3033e56d9
thomaseade <eadethomas95@gmail.com>
2023-03-14 19:15:55 +0100

    mmodificar funcion suma

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-function suma(op1, op2) {
-    return op1 + op2;
+function suma(op1, op2, op3) {
+    return op1 + op2 +op3;
   }
   
   function resta(op1, op2) {
--- run.js
+++ run.js
@@ -1,6 +1,6 @@
 let myLib = require("./index");
 
-console.log(myLib.suma(7, 1));
+console.log(myLib.suma(7, 1, 3));
 console.log(myLib.resta(3, 1));
 console.log(myLib.producto(2, 3));
 console.log(myLib.division(8, 2));
